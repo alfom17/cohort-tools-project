516fc60791911a7c4318a22ae35309e89ddbbf9e
Alfonso <alfonsomanuel2000@gmail.com>
2024-05-20 18:53:17 +0200

    ahi vamos

--- server/app.js
+++ server/app.js
@@ -1,7 +1,12 @@
+require('dotenv').config()
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
+
+const cohorts = require('./cohorts.json')
+const students = require('./students.json')
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -16,6 +21,7 @@ const app = express();
 // Research Team - Set up CORS middleware here:
 // ...
 app.use(express.json());
+app.use(cors())
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
@@ -29,6 +35,14 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+
+app.get('/api/cohorts',(req, res)=>{
+  res.json(cohorts)
+})
+
+app.get('/api/students',(req, res)=>{
+  res.json(students)
+})
 
 // START SERVER
 app.listen(PORT, () => {
